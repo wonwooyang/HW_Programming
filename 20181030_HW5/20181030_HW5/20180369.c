025466a8ff37cc82fb2142bb2b1186ac89de40df
wonwooyang <39733670+wonwooyang@users.noreply.github.com>
2018-10-30 23:10:25 +0900

    Add files via upload
    recursive + static 둘다 추가
    + 번호 순 맞춤

--- 20181030_HW5/20181030_HW5/20180369.c
+++ 20181030_HW5/20181030_HW5/20180369.c
@@ -4,36 +4,46 @@
 
 E-class 과제
 	입력 : 없음
-	출력 : 3~45 까지의 모든 홀수 fibo 출력
-
-
+	출력 : 3~45 번째까지의 모든 홀수번째 fibo 출력
 */
+static int f2, f3;
+static int f1= 1;
 int rfibo(int n);
 int sfibo(int n);
 
 void main() {
-	int start = 4, end = 45;
+	int start = 4, end = 45, rf = 0, sf = 0, num = 0;
 	printf("\n20180369 양원우\n");
-	printf("\n 3 ~ 45 사이의 모든 홀수번째 피보나치 수열을 출력합니다\n");
+	printf("\n 3 에서 45 번째 까지의 모든 홀수번째 피보나치 수열을 출력합니다\n");
+	printf("\nn번째\tRecursive\tStatic\n");
 
-	for (int i = 3; i <= end; i += 2) {
-		printf("\n%d : ", i);
-		printf("%d", rfibo(i));
+	for (int i = 0; i <= 45; i ++) {
+		rf = rfibo(i);
+		sf = sfibo(i);
+		num = i+2;
+		if (num%2==1) {
+			printf("\n%d\t", num);
+			printf("%d\t\t", rf);
+			printf("%d", sf);
+		}
 	}
-
-	/*
-	// 3 ~ 45 사이의 피보나치 수열 출력
-	while (rfibo(start) <= end) {
-		printf("%d  ", rfibo(start));
-		start++;
-	}
-	*/
 	printf("\n");
 }
 
 int rfibo(int n) {
 	if (n < 2) {
-		return n;
+		return 1;
 	}
 	return rfibo(n - 1) + rfibo(n - 2);
+}
+
+int sfibo(int n) {
+	//static int f1, f2, f3;
+	//초기값 > 0,1 을 넣어줘야함
+	//f2 = 1;
+	f3 = f1 + f2;
+	f1 = f2;
+	f2 = f3;
+
+	return f3;
 }
