4a005fa390884f53f95a4dcaada8b6757e950bf8
wonwooyang <wonwooyang@naver.com>
2018-11-11 18:28:20 +0900

    HW7 finished

--- 20181108_HW7/20181108_HW7/20180369.c
+++ 20181108_HW7/20181108_HW7/20180369.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
-#include <math.h>
 /*
-Sum(자기끼리)
-matrix의 index를 사용해서 > A[i][j]
+입력은 3x3 행렬을 입력합니다.
 
-Multiple
-martix의 pointer를 사용해서 > ptr
+ -출력은 입력된 행렬의 연산 결과를 출력합니다.
 
+  -행렬 1개만 입력받아 같은 행렬 2개로 계산하거나 2개 다 입력받아 계산하거나 상관 없습니다.
 
-pointer 를 사용해서 2X2 매트릭스 표현(책 확인)
-
-매트릭스 이름이 곧 주소값
-매트릭스 선언은 main 에서만
-sum(a,b,c)
-mult(a,b,c)
+  -행렬의 합은 Array Indexing, 곱은 Pointer 방식으로 접근해야 합니다. (Parameter는 행렬로 받아도 문제 없음)
 */
 int indexAddMat(int mat1[3][3], int mat2[3][3]);
 int ptrProdMat(mat1, mat2);
 
-main() {
+void main() {
 	int mat1[3][3], mat2[3][3];
-	
+
 	printf_s("\n20180369 양원우\n");
-	printf_s("\n3X3 Matrix 을 입력\n");
+	printf_s("\n==3X3 Matrix 을 입력==\n");
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			printf_s("%d번째 줄 %d번째 칸 : ", i, j);
+			printf_s("%d번째 행 %d번째 열 : ", i, j);
 			scanf_s("%d", &mat1[i][j]);
 		}
 	}
@@ -33,23 +26,28 @@ main() {
 	printf_s("\n==입력 값==\n");
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			printf_s("%d  ", mat1[i][j]);
+			printf_s("%3d  ", mat1[i][j]);
 		}
 		printf_s("\n");
 	}
 
 	indexAddMat(mat1, mat2);
-	printf_s("\n==Sum==\n");
+	printf_s("\n==Self Sum==\n");
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			printf_s("%d  ", mat2[i][j]);
+			printf_s("%3d  ", mat2[i][j]);
 		}
 		printf_s("\n");
 	}
 
-	printf_s("\n==Product==\n");
-
-	printf_s("");
+	ptrProdMat(mat1, mat2);
+	printf_s("\n==Self Product==\n");
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf_s("%3d  ", mat2[i][j]);
+		}
+		printf_s("\n");
+	}
 }
 
 int indexAddMat(int mat1[3][3], int mat2[3][3]) {
@@ -58,8 +56,20 @@ int indexAddMat(int mat1[3][3], int mat2[3][3]) {
 			mat2[i][j] = mat1[i][j] + mat1[i][j];
 		}
 	}
+	return 0;
 }
 
-int ptrProdMat(mat1, mat3) {
+int ptrProdMat(mat1, mat2) {
+	int(*pmat1)[3] = mat1;
+	int(*pmat2)[3] = mat2;
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			*(*(pmat2 + i) + j) = 0; // mat2 에 sum 연산값이 들어가있음 >> 0으로 초기화
+			for (int k = 0; k < 3; k++) {
+				*(*(pmat2 + i) + j) += (*(*(pmat1 + i) + k) * *(*(pmat1 + k) + j));				
+			}
+		}
+	}
 	return 0;
 }
