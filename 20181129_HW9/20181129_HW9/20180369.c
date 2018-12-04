2c3b0271aed83fd24f0a9fe6eb342abeda21ce3b
wonwooyang <wonwooyang@naver.com>
2018-12-04 23:26:48 +0900

    20181204 HW9 done

--- 20181129_HW9/20181129_HW9/20180369.c
+++ 20181129_HW9/20181129_HW9/20180369.c
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,36 +6,26 @@ struct node {
 	struct node *next;
 };
 void prtnd(struct node* head);
+void prtndM(struct node* head, int mid);
+void prtndR(struct node* head, int last);
+void delodd(struct node* head, int size);
 
 void main() {
+	printf_s("\n20180369 양원우\n");
 	typedef struct node node;
 	node *head = NULL, *nd = NULL, *last = NULL;
-	int key = 0, count = 0;
-	char inp[10];
-
+	int key = 0, count = 0, num = 0, eoeo=0;
+	printf("\n== 정수를 입력하세요 ==\n== Ctrl+Z, Enter 를 3번 반복하면 종료됩니다 ==\n");
 	while (1) {
-		printf("\n정수 입력 (현재 %d 개)\n", count);
-		/*
-		VS말고 DEV c++ 에서 하면 돌아간다 함
-		count=scanf("%d", &key);
-		//printf("key : %d \n", scanf("%d", &key));
-		if (count == EOF) {
-			printf_s("EOF");
-			exit(0);
+		if (scanf_s("%d", &num) == EOF) {
+			break;
 		}
-		*/
-
-
-		if (gets_s(inp, 10) == EOF) {
-			printf("!");  break;
-		}
-
 		nd = (node*)malloc(sizeof(node));
-		nd->value = atoi(inp);
+		nd->value = num;
 		nd->next = NULL;
 
 		if (head == NULL) {
-			head = &nd;
+			head = nd;
 		}
 		else {
 			last->next = nd;
@@ -45,39 +33,63 @@ void main() {
 		last = nd;
 		count++;
 	}
-
+	printf_s("\n==입력 순서 출력==\n");
 	prtnd(head);
+	printf_s("\n\n==자료의 개수==\n %2d", count);
+	printf_s("\n\n==중간 위치에 있는 자료의 값==\n");
+	prtndM(head, count / 2);
+	printf_s("\n\n==입력 역순 출력==\n");
+	prtndR(head, count-1);
+	printf_s("\n\n==홀수번째 삭제(0번째 부터 시작)==\n");
+	delodd(head, count);
+	prtnd(head);
+	printf_s("\n\n");
 }
+
 void prtnd(struct node* head) {
 	struct node *cur = head;
-	printf("\n");
-	while (cur->next) {
+	while (cur) {
+		printf(" %2d", cur->value);
 		cur = cur->next;
-		printf("%d  ", cur->value);
 	}
-
 }
-/*LAB 8 동적 메모리1 (자기참조 구조체)
-void main() {
-
-	typedef struct node node;
-
-	node *head = NULL, *first = NULL, *second = NULL;
-	first = (node*)malloc(sizeof(node));
-	second = (node*)malloc(sizeof(node));
-
-	first->value = 11;
-	second->value = 90;
-
-	head = &first;
-	first->next = &second;
-	second->next = NULL;
-
-	printf_s("\nHead 값 : %d", head);
-	printf_s("\nFirst node : \tAddr : %d\tvalue : %d \tnext : %d", &first, first->value, first->next);
-	printf_s("\nSecond node : \tAddr : %d\tvalue : %d \tnext : %d", &second, second->value, second->next);
-
-	free(first);
-	free(second);
+void prtndM(struct node* head, int mid) {
+	struct node *cur = head;
+	int cnt = 0;
+	while (cur) {
+		if (cnt == mid) {
+			printf(" %2d", cur->value);
+		}		
+		cur = cur->next;
+		cnt++;
+	}
 }
-*/
+void prtndR(struct node* head, int last) {
+	struct node *cur = head;
+	int cnt = 0;
+	while (last >= 0) {
+		while (cur) {
+			if (cnt == last) {
+				printf(" %2d", cur->value);
+			}
+			cur = cur->next;
+			cnt++;
+		}
+		cur = head;
+		cnt = 0;
+		last--;
+	}	
+}
+void delodd(struct node* head, int size) {
+	struct node *cur = head;
+	int cnt = 0;
+	while (cur->next != NULL) {
+		if (cnt % 2 == 0) {
+			cur->next = cur->next->next;
+			size--;
+			cnt--;
+		}
+		cnt++;
+		cur = cur->next;
+	}
+}
